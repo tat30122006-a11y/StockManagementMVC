e748be5dac98c8cfa45fce451bc863cc1707bbc1
Anh Tuấn Trần <TAT30122006@outlook.com.vn>
2026-05-14 15:00:28 +0700

    Tạo trang đăng nhập đăng ký

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockManagementMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
