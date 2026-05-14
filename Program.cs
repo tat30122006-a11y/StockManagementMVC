2ab7dbe314f723ba05a6a36497c95f6438877532
Anh Tuấn Trần <TAT30122006@outlook.com.vn>
2026-05-14 09:48:18 +0700

    Kết nối database sql server

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StockManagementMVC.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Connected sql
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ConnectedDb"]);
+});
 
 var app = builder.Build();
 
---
+++ Repository/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockManagementMVC.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
